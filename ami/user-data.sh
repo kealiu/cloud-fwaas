d7836af4342f02204c5a20b32b1cb5e46aa84d10
keliu <keilu@nwcdcloud.com>
2024-01-30 13:29:57 +0000

    bugfix for suricata userdata

--- ami/user-data.sh
+++ ami/user-data.sh
@@ -2,7 +2,7 @@
 # Copyright 2024 ke.liu#foxmail.com
 
 sudo add-apt-repository ppa:oisf/suricata-stable
-sudo apt-get update -y && sudo apt-get install -y jq curl nginx unzip suricata grepcidr
+sudo apt-get update -y && sudo apt-get install -y jq curl nginx unzip suricata suricata-update grepcidr
 
 # aws cli
 cd ${HOME}
@@ -57,10 +57,26 @@ sudo cp ${TMPGENEVESH} ${GENEVESH}
 sudo chmod +x ${GENEVESH}
 rm ${TMPGENEVESH}
 
-
 # setup script
 TMPCRON=/tmp/crontabl.tmp
 sudo bash -c "crontab -l > ${TMPCRON}"
 sudo bash -c "echo '@reboot /usr/local/bin/geneve.sh' > ${TMPCRON}"
+sudo bash -c "echo '0 8 * * * suricata-update' >> ${TMPCRON}"
 sudo crontab ${TMPCRON}
 sudo rm -rf ${TMPCRON}
+
+# config suricata
+sudo sed -i 's/LISTENMODE=.*/LISTENMODE=nf-queue/' /etc/default/suricata
+iface=$(ip route get 1 | awk '{print $5}')
+sudo sed  -i "s/IFACE=.*/IFACE=${iface}/" /etc/default/suricata
+
+sudo bash -c "echo 'modify-conf: /etc/suricata/modify.conf'  > /etc/suricata/update.yaml"
+sudo bash -c "echo 're:. ^alert drop' >  /etc/suricata/modify.conf"
+
+sudo suricata-update update-sources
+for src in $(suricata-update list-sources | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g" | awk '/Name/ {print $2}')
+do
+   sudo suricata-update enable-source ${src} secret-code=Free
+done
+sudo suricata-update
+
